e5ef4c4abcb4fb4a20918ac20b3b5e0491c4c6f0
poiley <benjpoile@gmail.com>
2025-03-12 11:39:05 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="parcelpending",
-    version="0.1.3",
+    version="1.0.0",
     author="poiley",
     author_email="benjpoile@gmail.com",
     description="A Python client for the ParcelPending website",
